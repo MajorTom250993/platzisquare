d5cc9335a823cf9ce3ed1828eb04e89551a7a911
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-11-29 22:37:02 -0600

    Directiva ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Platzisquare';
   lugares:any = [
-    { nombre: 'Florería la Gardenía' },
-    { nombre: 'Donas la pasadita' },
-    { nombre: 'Veterinaría Huellitas Felices' },
-    { nombre: 'Florería la Gardenía' },
-    { nombre: 'Donas la pasadita' },
-    { nombre: 'Veterinaría Huellitas Felices' }
+    { active: false, nombre: 'Florería la Gardenía' },
+    { active: true, nombre: 'Donas la pasadita' },
+    { active: false, nombre: 'Veterinaría Huellitas Felices' },
+    { active: true, nombre: 'Las Gatortas' },
+    { active: false, nombre: 'Cafebrería' },
+    { active: true, nombre: 'La Dosis' }
 ];
 
   constructor() {
