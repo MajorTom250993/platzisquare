5402202550bbf100094a59291486d91609a9906e
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-11-30 17:45:34 -0600

    Directiva ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Platzisquare';
   lugares:any = [
-    { active: false, nombre: 'Florería la Gardenía' },
-    { active: true, nombre: 'Donas la pasadita' },
-    { active: false, nombre: 'Veterinaría Huellitas Felices' },
-    { active: true, nombre: 'Las Gatortas' },
-    { active: false, nombre: 'Cafebrería' },
-    { active: true, nombre: 'La Dosis' }
+    { cercania: 1, distancia: 1, active: false, nombre: 'Florería la Gardenía' },
+    { cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita' },
+    { cercania: 2, distancia: 5, active: false, nombre: 'Veterinaría Huellitas Felices' },
+    { cercania: 3, distancia: 10, active: true, nombre: 'Las Gatortas' },
+    { cercania: 3, distancia: 35, active: false, nombre: 'Cafebrería' },
+    { cercania: 3, distancia: 120, active: true, nombre: 'La Dosis' }
   ];
   lat: number = 19.4182086;
   lng: number = -99.1616303;
