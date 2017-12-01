b8b1c093cfb2c65aa563bdfacbcd9849cfb0752b
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-11-30 18:29:36 -0600

    Directva de atributo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Platzisquare';
-  lugares:any = [
-    { cercania: 1, distancia: 1, active: false, nombre: 'Florería la Gardenía' },
-    { cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita' },
-    { cercania: 2, distancia: 5, active: false, nombre: 'Veterinaría Huellitas Felices' },
-    { cercania: 3, distancia: 10, active: true, nombre: 'Las Gatortas' },
-    { cercania: 3, distancia: 35, active: false, nombre: 'Cafebrería' },
-    { cercania: 3, distancia: 120, active: true, nombre: 'La Dosis' }
+  lugares: any = [
+    { plan: 'gratis', cercania: 1, distancia: 1, active: false, nombre: 'Florería la Gardenía' },
+    { plan: 'pagado', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita' },
+    { plan: 'gratis', cercania: 2, distancia: 5, active: false, nombre: 'Veterinaría Huellitas Felices' },
+    { plan: 'pagado', cercania: 3, distancia: 10, active: true, nombre: 'Las Gatortas' },
+    { plan: 'gratis', cercania: 3, distancia: 35, active: false, nombre: 'Cafebrería' },
+    { plan: 'pagado', cercania: 3, distancia: 120, active: true, nombre: 'La Dosis' }
   ];
   lat: number = 19.4182086;
   lng: number = -99.1616303;
