9081a7bc7c60e96c42509775a4e06e9c9c9f1827
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-12-03 17:21:54 -0600

    Implementación de rutas en el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Platzisquare';
-  lugares: any = [
-    { plan: 'gratis', cercania: 1, distancia: 1, active: false, nombre: 'Florería la Gardenía' },
-    { plan: 'pagado', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita' },
-    { plan: 'gratis', cercania: 2, distancia: 5, active: false, nombre: 'Veterinaría Huellitas Felices' },
-    { plan: 'pagado', cercania: 3, distancia: 10, active: true, nombre: 'Las Gatortas' },
-    { plan: 'gratis', cercania: 3, distancia: 35, active: false, nombre: 'Cafebrería' },
-    { plan: 'pagado', cercania: 3, distancia: 120, active: true, nombre: 'La Dosis' }
-  ];
-  lat: number = 19.4182086;
-  lng: number = -99.1616303;
-
   constructor() {
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,32 @@ import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { ResaltarDirective } from './directives/resaltar.directive';
 import { ContarClicksDirective } from './directives/contar-clicks.directive';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
 
+const appRoutes: Routes = [
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent}
+  {path: 'detalle', component: DetalleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ResaltarDirective,
-    ContarClicksDirective
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCggvxBjxpntoMMf7zkLqSxgGjw5HzBj_4'
-    })
+    }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
