4174d3e18b46111f4c6503e4455409a731adafb9
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-12-03 14:07:16 -0600

    Host Listeners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResaltarDirective
+    ResaltarDirective,
+    ContarClicksDirective
   ],
   imports: [
     BrowserModule,
