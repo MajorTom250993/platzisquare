96eb12ee612c5e8a5aebb90be81a36010c5e8bd4
Yonatan A. Rojas Serrano <yonatanrojas09@gmail.com>
2017-12-03 23:35:52 -0600

    parametros en rutas

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LugaresComponent implements OnInit {
   title = 'Platzisquare';
   lugares: any = [
-    { plan: 'gratis', cercania: 1, distancia: 1, active: false, nombre: 'Florería la Gardenía' },
-    { plan: 'pagado', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita' },
-    { plan: 'gratis', cercania: 2, distancia: 5, active: false, nombre: 'Veterinaría Huellitas Felices' },
-    { plan: 'pagado', cercania: 3, distancia: 10, active: true, nombre: 'Las Gatortas' },
-    { plan: 'gratis', cercania: 3, distancia: 35, active: false, nombre: 'Cafebrería' },
-    { plan: 'pagado', cercania: 3, distancia: 120, active: true, nombre: 'La Dosis' }
+    { id: 1, plan: 'gratis', cercania: 1, distancia: 1, active: true, nombre: 'Florería la Gardenía' },
+    { id: 2, plan: 'pagado', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita' },
+    { id: 3, plan: 'gratis', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaría Huellitas Felices' },
+    { id: 4, plan: 'pagado', cercania: 3, distancia: 10, active: true, nombre: 'Las Gatortas' },
+    { id: 5, plan: 'gratis', cercania: 3, distancia: 35, active: true, nombre: 'Cafebrería' },
+    { id: 6, plan: 'pagado', cercania: 3, distancia: 120, active: true, nombre: 'La Dosis' }
   ];
   lat: number = 19.4182086;
   lng: number = -99.1616303;
